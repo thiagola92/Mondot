42742a874b6b8a55049bccfc0d135b0b058a27d3
thiagola92 <thiagola92@gmail.com>
2021-05-09 18:06:58 -0300

    Move Connection to a scene in utility folder

--- scenes/utility/connection/connection.gd
+++ scenes/utility/connection/connection.gd
@@ -24,25 +24,31 @@ func _erase_unnecessary_fields(connection : Dictionary):
 			connection.erase(k)
 
 
+func _add_missing_fields(connection : Dictionary):
+	for key in example.keys():
+		if not key in connection:
+			connection[key] = example[key]
+
+
 func _are_types_valid(connection : Dictionary):
-	for field in connection.keys():
-		var current_type = $Type.of(connection[field])
-		var expected_type = $Type.of(example[field])
+	for key in connection.keys():
+		var current_type = $Type.of(connection[key])
+		var expected_type = $Type.of(example[key])
 		
 		if current_type != expected_type:
 			$Alert.message('Field: %s \nExpected: %s \nReceived: %s' % [
-				field, expected_type, current_type
+				key, expected_type, current_type
 			])
 			
 			return  false
-			
 	return true
 
 
 func load_connection(connection : Dictionary):
 	_erase_unnecessary_fields(connection)
+	_add_missing_fields(connection)
 	
-	if not _are_types_valid(connection):
+	if _are_types_valid(connection) == false:
 		return
 
 	print(connection)
