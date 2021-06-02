74cac30427e25afbbc5604078afe9ff664c2b90b
thiagola92 <thiagola92@gmail.com>
2021-06-01 23:08:00 -0300

    Refactory Connection methods

--- scenes/utility/connection.gd
+++ scenes/utility/connection.gd
@@ -1,51 +1,40 @@
 extends Reference
 
-const _Type = preload("res://scenes/utility/_type.gd")
+const Type = preload("res://scenes/utility/type.gd")
 const ConnectionParseResult = preload("res://scenes/utility/connection_parse_result.gd")
 
-const example = {
-	"_type_": 1,
-	"name": "",
-	"host": "",
-	"port": ""
+const structure = {
+	"name": [TYPE_STRING],
+	"host": [TYPE_STRING],
+	"port": [TYPE_INT]
 }
 
-static func _erase_unnecessary_fields(connection : Dictionary):
-	for k in connection.keys():
-		if not k in example:
-			connection.erase(k)
+
+static func _get_connection_values(connection : Dictionary, parse_result : ConnectionParseResult):
+	parse_result.result = {
+		'name': connection['name'],
+		'host': connection.get('host', '127.0.0.1'),
+		'port': connection.get('port', 27017),
+	}
 
 
-static func _add_missing_fields(connection : Dictionary):
-	for key in example.keys():
-		if not key in connection:
-			connection[key] = example[key]
-
-
-static func _are_types_valid(parse_result : Dictionary):
-	var connection = parse_result.result
-	
-	for key in connection.keys():
-		var current_type = _Type.of(connection[key])
-		var expected_type = _Type.of(example[key])
-		
-		if current_type != expected_type:
+static func _are_types_valid(connection : Dictionary, parse_result : ConnectionParseResult):
+	for key in structure.keys():
+		if not typeof(connection.get(key)) in structure[key]:
 			parse_result.error = ERR_INVALID_DATA
-			parse_result.error_string = 'Field: %s \nExpected: %s \nReceived: %s' % [
-				key, expected_type, current_type
-			]
+			parse_result.error_string = 'Unexpected type %s in field %s' % [Type.of(connection[key]), key]
 			
 			return false
 	return true
 
 
-static func parse(connection : Dictionary):
-	var parse_result = ConnectionParseResult.new(connection)
+static func parse(conn : Dictionary):
+	var connection = conn.duplicate(true)
+	var parse_result = ConnectionParseResult.new()
 	
-	_erase_unnecessary_fields(parse_result.result)
-	_add_missing_fields(parse_result.result)
-	
-	if _are_types_valid(parse_result) == false:
+	if _are_types_valid(connection, parse_result) == false:
 		return parse_result
+	
+	_get_connection_values(connection, parse_result)
 
 	return parse_result
--- scenes/utility/connection_parse_result.gd
+++ scenes/utility/connection_parse_result.gd
@@ -1,33 +1,5 @@
 extends Reference
 
-var error = OK setget set_error, get_error
-var error_string = '' setget set_error_string, get_error_string
-var result = {} setget set_result, get_result
-
-
-func _init(connection : Dictionary):
-	result = connection.duplicate(true)
-
-
-func set_error(value : int) -> void:
-	error = value
-
-
-func get_error() -> int:
-	return error
-
-
-func set_error_string(value : String) -> void:
-	error_string = value
-
-
-func get_error_string() -> String:
-	return error_string
-
-
-func set_result(value : Dictionary) -> void:
-	result = value
-
-
-func get_result() -> Dictionary:
-	return result
+var error : int = OK
+var error_string  : String = ''
+var result : Dictionary = {}
