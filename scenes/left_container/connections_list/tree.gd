7cbc324add05742357135091c817c58de031ab3d
thiagola92 <thiagola92@gmail.com>
2021-06-13 12:30:43 -0300

    Finish import implementation

--- scenes/left_container/connections_list/tree.gd
+++ scenes/left_container/connections_list/tree.gd
@@ -7,11 +7,6 @@ func _ready():
 	root = create_item()
 
 
-func load_connections(connections : Array):
-	for connection in connections:
-		print(connection)
-
-
 func _is_name_used(parent : TreeItem, name : String):
 	var child = parent.get_children()
 	
@@ -32,45 +27,42 @@ func _get_available_name(name : String, counter_name : String):
 	return name.format({counter_name: count})
 
 
-func _create_node(parent : TreeItem, name : String, metadata : Dictionary):
+func _create_node(parent : TreeItem, metadata : Dictionary):
 	var child = create_item(parent)
-	child.set_text(0, name)
+	child.set_text(0, metadata['name'])
 	child.set_metadata(0, metadata)
+	
+	if metadata['__type__'] == ConnectionType.FOLDER:
+		for item in metadata['connections']:
+			_create_node(child, item)
 	
 	scroll_to_item(child)
 
 
-func _create_folder(parent : TreeItem, name : String):
-	_create_node(parent, name, {
-		'_type_': ConnectionType.FOLDER,
-		'name': name
-	})
-
-
-func _create_connection(parent : TreeItem, name : String):
-	_create_node(parent, name, {
-		'_type_': ConnectionType.CONNECTION,
-		'name': name
+func _on_NewConnection_pressed():
+	var name = _get_available_name('New connection {count}', 'count')
+	_create_node(root, {
+		'__type__': ConnectionType.CONNECTION,
+		'name': name,
 	})
 
 
 func _on_NewFolder_pressed():
 	var name = _get_available_name('New folder {count}', 'count')
-	_create_folder(root, name)
-
-
-func _on_NewConnection_pressed():
-	var name = _get_available_name('New connection {count}', 'count')
-	_create_connection(root, name)
+	_create_node(root, {
+		'__type__': ConnectionType.FOLDER,
+		'name': name,
+		'connections': [],
+	})
 
 
 func _on_Tree_item_activated():
 	var metadata = get_selected().get_metadata(0)
 	
-	if metadata["_type_"] == ConnectionType.CONNECTION:
-		print("connect with mongo") # TODO
+	if metadata["__type__"] == ConnectionType.CONNECTION:
+		print("open connection settings") # TODO
 
 
 func _on_Import_connections_loaded(connections):
 	for connection in connections:
-		_create_node(root, connection['name'], connection)
+		_create_node(root, connection)
