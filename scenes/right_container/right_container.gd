4e98ddc4e1926cd13ebf8c97edc2ceb88eaf7304
thiagola92 <thiagola92@gmail.com>
2021-08-19 06:45:58 -0300

    Tabs hide the content instead of removing.
    Removing would cause them to be removed from the tree. Which would stop
    the python process. Hiding will let the process running in the background.
    
    This means that two python process can be running at the same time
    without one affecting other.

--- scenes/right_container/right_container.gd
+++ scenes/right_container/right_container.gd
@@ -1,7 +1,6 @@
 extends VBoxContainer
 
 const startup = preload("res://scenes/right_container/startup/Startup.tscn")
-const contents = []
 
 
 func _ready():
@@ -10,61 +9,60 @@ func _ready():
 
 func add_tab(name : String, content : Node):
 	_add_tab(name, content)
-	
-	if _exist_only_one_tab():
-		_change_to_tab(0)
+	_change_to_last_tab()
 
 
 func _add_tab(name : String, content):
-	contents.append(content)
+	$Panel.add_child(content)
 	$TabsBg/Tabs.add_tab(name)
 
 
-func _exist_only_one_tab():
-	return len(contents) == 1
+func _change_to_last_tab():
+	var last_tab = $TabsBg/Tabs.get_tab_count() - 1
+	_change_to_tab(last_tab)
 
 
 func _change_to_tab(tab : int):
-	$TabsBg/Tabs.current_tab = tab
-	$TabsBg/Tabs.emit_signal("tab_changed", tab)
+	if tab >= 0:
+		$TabsBg/Tabs.current_tab = tab
+		$TabsBg/Tabs.emit_signal("tab_changed", tab)
 
 
-func _remove_panel_content():
-	if $Panel.get_child_count() > 0:
-		$Panel.remove_child($Panel.get_child(0))
+func _on_Tabs_tab_changed(tab : int):
+	# The signal "tab_changed" doesn't tell the previous tab.
+	# So we hide all contents before showing the new tab.
+	_hide_all_panel_content()
+	_show_panel_content(tab)
 
 
-func _add_panel_content(content):
-	$Panel.add_child(content)
+func _hide_all_panel_content():
+	for child in $Panel.get_children():
+		child.hide()
+
+
+func _show_panel_content(tab : int):
+	$Panel.get_child(tab).show()
+
+
+func _on_Tabs_tab_close(tab : int):
+	_remove_panel_content(tab)
+	_remove_tab(tab)
+	_change_to_last_tab()
+
+
+func _remove_panel_content(tab : int):
+	var child = $Panel.get_child(tab)
+	child.queue_free()
 
 
 func _remove_tab(tab : int):
 	$TabsBg/Tabs.remove_tab(tab)
-	contents.remove(tab)
-
-
-func _exist_others_tabs():
-	return len(contents) > 0
-
-
-func _reposition_content(idx_from : int, idx_to : int):
-	var content = contents[idx_from]
-	contents.remove(idx_from)
-	contents.insert(idx_to, content)
-
-
-func _on_Tabs_tab_changed(tab : int):
-	_remove_panel_content()
-	_add_panel_content(contents[tab])
-
-
-func _on_Tabs_tab_close(tab : int):
-	_remove_panel_content()
-	_remove_tab(tab)
-	
-	if _exist_others_tabs():
-		_change_to_tab($TabsBg/Tabs.current_tab)
 
 
 func _on_Tabs_reposition_active_tab_request(idx_to : int):
 	_reposition_content($TabsBg/Tabs.current_tab, idx_to)
+
+
+func _reposition_content(idx_from : int, idx_to : int):
+	var child = $Panel.get_child(idx_from)
+	$Panel.move_child(child, idx_to)
