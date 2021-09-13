071832718d623a9cc0720ab6dc7070e2b9f4d029
thiagola92 <thiagola92@gmail.com>
2021-09-12 21:09:09 -0300

    Update Alert popup to store message in TextEdit Node
    Now the base node is WindowDialog.
    Had to reproduce AcceptPopup Ok button and used TextEdit instead of Label.
    
    This should fix any problem in showing LONG messages because TextEdit
    let you scroll. Anyway, i am letting the user resize the window if the
    intention is see all message in the screen

--- scenes/utility/alert/alert.gd
+++ scenes/utility/alert/alert.gd
@@ -1,11 +1,19 @@
-extends AcceptDialog
+extends WindowDialog
+
+var base_size = rect_size
 
 
 func _ready():
-	pass
+	# For test purpose only
+	if $"." in get_tree().get_root().get_children():
+		message("Test message")
 
 
 func message(text : String):
-	dialog_text = text
+	$Container/Message.text = text
+	rect_size = base_size
 	popup_centered()
-	set_as_minsize()
+
+
+func _on_Ok_pressed():
+	hide()
