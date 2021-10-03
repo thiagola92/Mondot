e015d7c2973c76366a0a9a0d4a9a242876e535a6
thiagola92 <thiagola92@gmail.com>
2021-10-03 04:32:35 -0300

    Let Godot optimize for low processor usage
    Mondot don't usually refresh the screen, so i'm setting to
    low_processor_usage_mode to true

--- scenes/main_container/main_container.gd
+++ scenes/main_container/main_container.gd
@@ -4,7 +4,7 @@ var Shell = preload("res://scenes/main_container/right_container/shell/Shell.tsc
 
 
 func _ready():
-	pass
+	OS.low_processor_usage_mode = true
 
 
 func _on_LeftContainer_shell_requested(uri : String, db : String, code : String, readonly : bool, hidden : bool):
